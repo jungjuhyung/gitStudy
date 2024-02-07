7dc4c65021fdeb9a1f63fccfa6ff26b0918c8f20
wngud01 <ramyun1537@gmail.com>
2024-02-07 10:03:36 +0900

    home1

--- src/com/ict/day01/Ex01.java
+++ src/com/ict/day01/Ex01.java
@@ -4,8 +4,8 @@ public class Ex01 {
 	public static void main(String[] args) {
 		System.out.println("hello~java");
 		System.out.println("hello~java");
-		System.out.println("hello~java");
-		System.out.println("hello~java");
-		System.out.println("hello~java");
+		System.out.println("home에서 수정");
+		System.out.println("home에서 수정");
+		System.out.println("home에서 수정");
 	}
 }
