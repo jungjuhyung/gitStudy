8a4b3e83b602deb3a6fc53852d78909a729b88e1
wngud01 <ramyun1537@gmail.com>
2024-02-07 09:43:11 +0900

    school1

---
+++ src/com/ict/day01/Ex01.java
@@ -0,0 +1,11 @@
+package com.ict.day01;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		System.out.println("hello~java");
+		System.out.println("hello~java");
+		System.out.println("hello~java");
+		System.out.println("hello~java");
+		System.out.println("hello~java");
+	}
+}
